3df8b9a98deb9c1c287f44324da7071367a7613e
Andy <andy@tramicuba.com>
2025-08-17 23:51:02 -0400

    Actualizaci├│n autom├ítica

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initPWA } from './modules/pwa.js';
-import { setupServices } from './modules/services.js';
-import { initModals } from './modules/modals.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initPWA();
-  setupServices();
-  initModals();
-}); 
